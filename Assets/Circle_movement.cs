61958e937f1ea7bd6f9184772af8c2324bc309ef
elHunity <elhunity@gmail.com>
2023-12-27 19:35:12 +0200

    New objtct.Normal basic movment

---
+++ Assets/Circle_movement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CircleMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Adjust this value to control the movement speed.
+
+    void Update()
+    {
+        // Get the horizontal input (left/right arrow keys or A/D keys)
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Calculate the movement vector
+        Vector2 movement = new Vector2(horizontalInput, 0f) * moveSpeed * Time.deltaTime;
+
+        // Move the object
+        transform.Translate(movement);
+    }
+}
