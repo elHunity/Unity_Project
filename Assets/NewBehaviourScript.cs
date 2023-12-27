0beb40979e5c7cdb050929fdb1045d080fc45469
elHunity <elhunity@gmail.com>
2023-12-27 16:58:21 +0200

    Simple movmevt issue

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log("NewBehaviourScript script has started.");
+        Debug.Log($"Horizontal: {horizontal}, Vertical: {vertical}");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float Horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movment = new Vector2(horizontal, vertical);
+        transform.Translate(movment * speed * Time.deltaTime);
     }
 }
